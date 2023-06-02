84d2524b0443487852002336a5a05e8dc6aeff38
Jiarui Li <118020229@link.cuhk.edu.cn>
2023-06-02 23:06:50 +0000

    feat: replacement script

--- run_experiment_replace.py
+++ run_experiment_replace.py
@@ -5,7 +5,7 @@ This script runs a pFedDef training on the FedEM model.
 from utils.utils import *
 from utils.constants import *
 from utils.args import *
-from run_experiment import * 
+from run_experiment import *
 
 from torch.utils.tensorboard import SummaryWriter
 
@@ -27,80 +27,95 @@ from transfer_attacks.Transferer import *
 from transfer_attacks.Args import *
 from transfer_attacks.TA_utils import *
 
-import numba 
+import numba
 import time
 from datetime import datetime
 import pytz
 
 
 if __name__ == "__main__":
-    newYorkTz = pytz.timezone("America/New_York") 
+    newYorkTz = pytz.timezone("America/New_York")
     timeInNewYork = datetime.now(newYorkTz)
     currentTimeInNewYork = timeInNewYork.strftime("%H:%M:%S")
 
     print("The current time in New York is:", currentTimeInNewYork)
-    
-    ## INPUT GROUP 1 - experiment macro parameters ##
-    # scale_set = [scale for scale in range(220, 241, 5)]
-    scale_set = np.arange(240, 251, 1)
 
-    exp_names = [f'rep_scale{int(i)}' for i in scale_set]
+    ## INPUT GROUP 1 - experiment macro parameters ##
+    atk_nums = [1, 3, 5, 10, 15, 20, 25, 30]
+    client_weights = np.load(
+        "/home/ubuntu/Documents/jiarui/experiments/multi_atker/client_weights.npy"
+    )
+
+    # calculate the theoretical best scale for each attack number
+    best_scales = [int(1 / client_weights[0:atk_num].sum()) for atk_num in atk_nums]
+    params = []
+    for idx, scale in enumerate(best_scales):
+        if scale - 4 <= 0:
+            for i in range(1, scale+1):
+                params.append((atk_nums[idx], i))
+            params.append((atk_nums[idx], scale+1))
+            params.append((atk_nums[idx], scale+2))
+        else:
+            for i in range(0, 10, 2):
+                params.append((atk_nums[idx], scale + i - 4))
+
+    exp_names = [f"atk_{atk_num}_scale{scale}" for atk_num, scale in params]
     exp_root_path = input("exp_root_path>>>>")
-    path_log = open(f"{exp_root_path}/path_log", mode = "w")
-    
+    path_log = open(f"{exp_root_path}/path_log", mode="w")
+
     n_learners = 1
     ## END INPUT GROUP 1 ##
 
-    
     for itt in range(len(exp_names)):
-        
         print("\n\nrunning trial:", itt)
-        
+
         ## INPUT GROUP 2 - experiment macro parameters ##
         args_ = Args()
-        args_.experiment = "cifar10"      # dataset name
-        args_.method = 'FedAvg'       # Method of training
+        args_.experiment = "cifar10"  # dataset name
+        args_.method = "FedAvg"  # Method of training
         args_.decentralized = False
         args_.sampling_rate = 1.0
         args_.input_dimension = None
         args_.output_dimension = None
-        args_.n_learners= n_learners      # Number of hypotheses assumed in system
-        args_.n_rounds = 150              # Number of rounds training takes place
+        args_.n_learners = n_learners  # Number of hypotheses assumed in system
+        args_.n_rounds = 150  # Number of rounds training takes place
         args_.bz = 128
         args_.local_steps = 1
         args_.lr_lambda = 0
-        args_.lr = 0.03                   # Learning rate
-        args_.lr_scheduler = 'multi_step'
+        args_.lr = 0.03  # Learning rate
+        args_.lr_scheduler = "multi_step"
         args_.log_freq = 20
-        args_.device = 'cuda'
-        args_.optimizer = 'sgd'
+        args_.device = "cuda"
+        args_.optimizer = "sgd"
         args_.mu = 0
         args_.communication_probability = 0.1
         args_.q = 1
         args_.locally_tune_clients = False
         args_.seed = 1234
         args_.verbose = 1
-        args_.logs_root = f'{exp_root_path}/{exp_names[itt]}/logs'
-        args_.save_path = f'{exp_root_path}/{exp_names[itt]}/weights'      # weight save path
+        args_.logs_root = f"{exp_root_path}/{exp_names[itt]}/logs"
+        args_.save_path = (
+            f"{exp_root_path}/{exp_names[itt]}/weights"  # weight save path
+        )
         # args_.load_path = f'/home/ubuntu/Documents/jiarui/experiments/{args_.method}/{args_.experiment}/replace/replace_fail_1/weights'
         # args_.load_path = f'/home/ubuntu/Documents/jiarui/experiments/fedavg/gt_epoch200/weights'
-        args_.load_path = f'/home/ubuntu/Documents/jiarui/experiments/FedAvg_adv/gt_1leaner_adv/weights/gt200'
+        args_.load_path = f"/home/ubuntu/Documents/jiarui/experiments/FedAvg_adv/gt_1leaner_adv/weights/gt200"
         # args_.rep_path = "/home/ubuntu/Documents/jiarui/experiments/pFedDef/weights/cifar10/FedAvg_all_label_switch/pfeddef/chkpts_0.pt"
         args_.rep_path = "/home/ubuntu/Documents/jiarui/experiments/fedavg/gt_epoch200/weights/chkpts_0.pt"
         args_.validation = False
+        args_.aggregation_op = "trimmed_mean"
 
         if itt == 0:
-            path_log.write(f'{args_.method}\n')
-        path_log.write(f'{exp_root_path}/{exp_names[itt]}\n')
+            path_log.write(f"{args_.method}\n")
+        path_log.write(f"{exp_root_path}/{exp_names[itt]}\n")
 
-        num_clients = 40                  # Number of clients to train with
-        num_classes = 10                  # Number of classes in the data set we are training with
+        num_clients = 40  # Number of clients to train with
+        num_classes = 10  # Number of classes in the data set we are training with
         ## END INPUT GROUP 2 ##
-        
 
         # Randomized Parameters
         Ru = np.ones(num_clients)
-        
+
         # Generate the dummy values here
         aggregator, clients = dummy_aggregator(args_, num_clients)
 
@@ -110,24 +125,21 @@ if __name__ == "__main__":
             aggregator.load_state(load_root)
 
             args_.n_rounds = 1
-            print("Update clients before training")
-            aggregator.update_clients()     # update the client's parameters immediatebly, since they should have an up-to-date consistent global model before training starts
+            aggregator.update_clients()  # update the client's parameters immediatebly, since they should have an up-to-date consistent global model before training starts
 
         # Perform label swapping attack for a set number of clients
-        args_.atk_count = 1
+        args_.atk_count = params[itt][0]
         args_.atk_round = 1
         atk_count = args_.atk_count
         atk_round = args_.atk_round
         for i in range(atk_count):
             client_weight = aggregator.clients_weights[i]
-            print(f"The {i}th client weight is {1/client_weight}")
-            print(f"all weights {aggregator.clients_weights}")
-            print(f"attacking with scale {scale_set[itt]}")
+            print(f"attacking with scale {params[itt][1]}")
             aggregator.clients[i].turn_malicious(
-                factor = scale_set[itt],  
-                attack = "replacement",
-                atk_round = args_.n_rounds - atk_round, # attack rounds in the end
-                replace_model_path = args_.rep_path
+                factor=params[itt][1],
+                attack="replacement",
+                atk_round=args_.n_rounds - atk_round,  # attack rounds in the end
+                replace_model_path=args_.rep_path,
             )
 
         # Train the model
@@ -135,12 +147,14 @@ if __name__ == "__main__":
         pbar = tqdm(total=args_.n_rounds)
         current_round = 0
 
-        aggregator.change_all_clients_status(range(1,num_clients), False)
-        
+        aggregator.change_all_clients_status(range(1, num_clients), False)
+
         while current_round < args_.n_rounds:
             print(f"Global round {current_round}")
-            global_learners_ensemble_copy = copy.deepcopy(aggregator.global_learners_ensemble)
-            
+            global_learners_ensemble_copy = copy.deepcopy(
+                aggregator.global_learners_ensemble
+            )
+
             if current_round == args_.n_rounds - atk_round:
                 save_root = os.path.join(args_.save_path, "before_rep")
                 os.makedirs(save_root, exist_ok=True)
@@ -148,33 +162,27 @@ if __name__ == "__main__":
 
             if current_round >= args_.n_rounds - atk_round:
                 print(f"Global Round {current_round} - Start Replacement!")
-                aggregator.mix(replace = True)
+                aggregator.mix(replace=True)
             else:
                 aggregator.mix()
-            
 
             if aggregator.c_round != current_round:
                 pbar.update(1)
                 current_round = aggregator.c_round
-          
 
         if "save_path" in args_:
             save_root = os.path.join(args_.save_path)
 
             os.makedirs(save_root, exist_ok=True)
             aggregator.save_state(save_root)
-            
-        save_arg_log(f_path = args_.logs_root, args = args_)
+
+        save_arg_log(f_path=args_.logs_root, args=args_)
         np.save(
-            f"{exp_root_path}/{exp_names[itt]}/client_dist_to_prev_gt_in_each_round.npy", 
-            np.array(
-                aggregator.client_dist_to_prev_gt_in_each_round
-            )
+            f"{exp_root_path}/{exp_names[itt]}/client_dist_to_prev_gt_in_each_round.npy",
+            np.array(aggregator.client_dist_to_prev_gt_in_each_round),
         )
-        
+
         del args_, aggregator, clients
         torch.cuda.empty_cache()
 
     path_log.close()
-
-            
